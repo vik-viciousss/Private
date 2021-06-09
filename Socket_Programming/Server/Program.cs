9ec649cd3362c7f35bcbc3e50ad08aeb80747c3e
Viktoria Marinova <64691525+vik-viciousss@users.noreply.github.com>
2021-06-09 23:06:14 +0300

    echoServer set

---
+++ Socket_Programming/Server/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var echo = new EchoServer();
+
+            echo.Start();
+
+            Console.WriteLine("Echo server running");
+            Console.ReadLine();
+
+        }
+    }
+}
