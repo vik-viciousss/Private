7b53abadb180df26a7dbf4ce0fa58b98a837000a
Viktoria Marinova <64691525+vik-viciousss@users.noreply.github.com>
2021-06-10 16:07:07 +0300

    created protocols

--- Socket_Programming/Client/Program.cs
+++ Socket_Programming/Client/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shared;
+using System;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
@@ -18,67 +19,67 @@ namespace Client
 
     class Program
     {
-        static async Task SendAsync<T>(NetworkStream networkStream, T message)
-        {
-            var (header, body) = Encode(message);
+        //static async Task SendAsync<T>(NetworkStream networkStream, T message)
+        //{
+        //    var (header, body) = Encode(message);
 
-            await networkStream.WriteAsync(header, 0, header.Length).ConfigureAwait(false);
-            await networkStream.WriteAsync(body, 0, body.Length).ConfigureAwait(false);
-        }
+        //    await networkStream.WriteAsync(header, 0, header.Length).ConfigureAwait(false);
+        //    await networkStream.WriteAsync(body, 0, body.Length).ConfigureAwait(false);
+        //}
 
-        static async Task<T> ReceiveAsync<T>(NetworkStream networkStream)
-        {
-            var headerBytes = await ReadAsync(networkStream, 4);
-            var bodyLength = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(headerBytes));
+        //static async Task<T> ReceiveAsync<T>(NetworkStream networkStream)
+        //{
+        //    var headerBytes = await ReadAsync(networkStream, 4);
+        //    var bodyLength = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(headerBytes));
 
-            var bodyBytes = await ReadAsync(networkStream, bodyLength);
+        //    var bodyBytes = await ReadAsync(networkStream, bodyLength);
 
-            return Decode<T>(bodyBytes);
-        }
+        //    return Decode<T>(bodyBytes);
+        //}
 
-        static (byte[] header, byte[] body) Encode<T>(T message)
-        {
-            var xs = new XmlSerializer(typeof(T));
+        //static (byte[] header, byte[] body) Encode<T>(T message)
+        //{
+        //    var xs = new XmlSerializer(typeof(T));
 
-            var sb = new StringBuilder();
-            var sw = new StringWriter(sb);
+        //    var sb = new StringBuilder();
+        //    var sw = new StringWriter(sb);
 
-            xs.Serialize(sw, message);
+        //    xs.Serialize(sw, message);
 
-            var bodyBytes = System.Text.Encoding.UTF8.GetBytes(sb.ToString());
-            var headerBytes = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(bodyBytes.Length));
+        //    var bodyBytes = System.Text.Encoding.UTF8.GetBytes(sb.ToString());
+        //    var headerBytes = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(bodyBytes.Length));
 
-            return (headerBytes, bodyBytes);
-        }
+        //    return (headerBytes, bodyBytes);
+        //}
 
-        static T Decode<T>(byte[] body)
-        {
-            var str = System.Text.Encoding.UTF8.GetString(body);
-            var sr = new StringReader(str);
-            var xs = new XmlSerializer(typeof(T));
+        //static T Decode<T>(byte[] body)
+        //{
+        //    var str = System.Text.Encoding.UTF8.GetString(body);
+        //    var sr = new StringReader(str);
+        //    var xs = new XmlSerializer(typeof(T));
 
-            return (T)xs.Deserialize(sr);
-        }
+        //    return (T)xs.Deserialize(sr);
+        //}
 
-        static async Task<byte[]> ReadAsync(NetworkStream networkStream, int bytesToRead)
-        {
-            var buffer = new byte[bytesToRead];
-            var bytesRead = 0;
+        //static async Task<byte[]> ReadAsync(NetworkStream networkStream, int bytesToRead)
+        //{
+        //    var buffer = new byte[bytesToRead];
+        //    var bytesRead = 0;
 
-            while (bytesRead < bytesToRead)
-            {
-                var bytesReceived = await networkStream.ReadAsync(buffer, bytesRead, (bytesToRead - bytesRead)).ConfigureAwait(false);
+        //    while (bytesRead < bytesToRead)
+        //    {
+        //        var bytesReceived = await networkStream.ReadAsync(buffer, bytesRead, (bytesToRead - bytesRead)).ConfigureAwait(false);
 
-                if (bytesReceived == 0)
-                {
-                    throw new Exception("Socket closed");
-                }
+        //        if (bytesReceived == 0)
+        //        {
+        //            throw new Exception("Socket closed");
+        //        }
 
-                bytesRead += bytesReceived;
-            }
+        //        bytesRead += bytesReceived;
+        //    }
 
-            return buffer;
-        }
+        //    return buffer;
+        //}
 
         static async Task Main(string[] args)
         {
@@ -101,12 +102,17 @@ namespace Client
             Console.WriteLine("Sending");
             Print(myMessage);
 
-            await SendAsync(networkStream, myMessage).ConfigureAwait(false);
+            var protocol = new XMLMessageProtocol();
+            await protocol.SendAsync(networkStream, myMessage).ConfigureAwait(false);
 
-            var responseMsg = await ReceiveAsync<MyMessage>(networkStream).ConfigureAwait(false);
+            //await SendAsync(networkStream, myMessage).ConfigureAwait(false);
+            //var responseMsg = await ReceiveAsync<MyMessage>(networkStream).ConfigureAwait(false);
+
+            var xDoc = await protocol.ReceiveAsync(networkStream).ConfigureAwait(false);
 
             Console.WriteLine("Received");
-            Print(responseMsg);
+            Console.WriteLine(xDoc.ToString());
+            //Print(responseMsg);
 
             Console.ReadLine();
         }
---
+++ Socket_Programming/Shared/JsonMessageProtocol.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Shared
+{
+    public class JsonMessageProtocol : Protocol<JObject>
+    {
+        protected override JObject Decode(byte[] message)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override byte[] EncodeBody<T>(T message)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
