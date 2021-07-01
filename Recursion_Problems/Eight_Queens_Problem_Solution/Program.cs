bacfe3ed304677cb4ed95870713eff82f7156424
Viktoria Marinova <64691525+vik-viciousss@users.noreply.github.com>
2021-07-01 20:51:04 +0300

    recursion-problem-solutions

---
+++ Recursion_Problems/Eight_Queens_Problem_Solution/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace Eight_Queens_Problem_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int queenCount = int.Parse(Console.ReadLine());
+
+            int[,] queens = new int[queenCount, queenCount];
+
+            Console.WriteLine(GetQueens(queens, 0));
+
+
+        }
+
+        static void PrintQueens(int[,] queens)
+        {
+            for (int row = 0; row < queens.GetLength(0); row++)
+            {
+                for (int col = 0; col < queens.GetLength(1); col++)
+                {
+                    if (queens[row,col] == 1)
+                    {
+                        Console.Write("Q" + " ");
+                    }
+                    if (queens[row, col] == 0)
+                    {
+                        Console.Write("_" + " ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        private static int GetQueens(int[,] queens, int row)
+        {
+            if (row == queens.GetLength(0))
+            {
+                PrintQueens(queens);
+                return 1;
+            }
+
+            int foundQueens = 0;
+
+            for (int col = 0; col < queens.GetLength(1); col++)
+            {
+                if (IsSafe(queens, row, col))
+                {
+                    queens[row, col] = 1;
+                    foundQueens += GetQueens(queens, row+1);
+                    queens[row, col] = 0;
+                }
+            }
+
+            return foundQueens;
+        }
+
+        private static bool IsSafe(int[,] queens, int row, int col)
+        {
+            for (int i = 1; i < queens.GetLength(0); i++)
+            {
+                if (row - i >= 0 && queens[row - i, col] == 1)
+                {
+                    return false;
+                }
+                if (col - i >= 0 && queens[row, col - i] == 1)
+                {
+                    return false;
+                }
+                if (row + i < queens.GetLength(0) && queens[row + i, col] == 1)
+                {
+                    return false;
+                }
+                if (col + i < queens.GetLength(0) && queens[row, col + i] == 1)
+                {
+                    return false;
+                }
+
+                if (col + i < queens.GetLength(0) && 
+                    row + i < queens.GetLength(0) &&
+                    queens[row + i, col + i] == 1)
+                {
+                    return false;
+                }
+                if (col - i >= 0 &&
+                    row + i < queens.GetLength(0) &&
+                    queens[row + i, col - i] == 1)
+                {
+                    return false;
+                }
+                if (col - i >= 0 && 
+                    row - i >= 0 && 
+                    queens[row - i, col - i] == 1)
+                {
+                    return false;
+                }
+                if (col + i < queens.GetLength(0) &&
+                    row - i >= 0 && 
+                    queens[row - i, col + i] == 1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
